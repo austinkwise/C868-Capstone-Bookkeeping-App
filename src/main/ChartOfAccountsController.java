8bba146ae4f38892759e97b1d09dcdba3823a5a0
Austin Wise <austin.k.wise@gmail.com>
2019-05-14 13:18:10 -0400

    finished out most of the screens and navigation.

--- src/main/ChartOfAccountsController.java
+++ src/main/ChartOfAccountsController.java
@@ -1,4 +1,15 @@
 package main;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ChartOfAccountsController {
+
+    @FXML
+    private void addAccount(){
+
+    }
 }
